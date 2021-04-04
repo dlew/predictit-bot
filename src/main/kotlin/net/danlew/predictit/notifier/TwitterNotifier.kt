97d025493659f13233bd3367f62894baafabc905
Dan Lew <danlew42@gmail.com>
2021-04-04 09:49:30 -0500

    Made it still work even without Twitter auth

--- src/main/kotlin/net/danlew/predictit/notifier/TwitterNotifier.kt
+++ src/main/kotlin/net/danlew/predictit/notifier/TwitterNotifier.kt
@@ -1,18 +1,35 @@
 package net.danlew.predictit.notifier
 
 import net.danlew.predictit.model.FormattedNotification
+import org.slf4j.LoggerFactory
 import twitter4j.StatusUpdate
+import twitter4j.TwitterAPIConfiguration
 import twitter4j.TwitterFactory
 
 
 class TwitterNotifier : Notifier {
 
+  private val logger = LoggerFactory.getLogger(TwitterNotifier::class.java)
+
   private val twitter = TwitterFactory.getSingleton()
 
   // TODO: We should refresh this once/day
-  private val apiConfiguration = twitter.apiConfiguration
+  private var apiConfiguration: TwitterAPIConfiguration? = null
+
+  init {
+    // We use "can hit Configuration endpoint" as a test for whether the auth credentials worked
+    try {
+      apiConfiguration = twitter.apiConfiguration
+    } catch (e: Exception) {
+      logger.error("Twitter4J is not configured properly; not tweeting output.", e)
+    }
+  }
 
   override fun notify(notifications: Set<FormattedNotification>) {
+    if (apiConfiguration == null) {
+      return
+    }
+
     notifications.forEach {
       tweet(it)
 
@@ -29,12 +46,11 @@ class TwitterNotifier : Notifier {
   private fun formatStatus(notification: FormattedNotification): String {
     val status = "${notification.text}\n\n"
     val shortStatus = "${notification.textShort}\n\n"
-    val urlLength = apiConfiguration.shortURLLengthHttps
+    val urlLength = apiConfiguration?.shortURLLengthHttps ?: 25
 
     if (status.length + urlLength < CHARACTER_LIMIT) {
       return status + notification.link
-    }
-    else {
+    } else {
       return shortStatus + notification.link
     }
   }
